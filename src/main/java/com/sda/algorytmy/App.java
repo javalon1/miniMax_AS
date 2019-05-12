c85c798495aa6e15c4ae87fb60e8a012c8fca96b
Anna Smolinska <anias05@tlen.pl>
2019-05-12 14:03:10 +0100

    miniMax ostatnia werscja

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,26 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int[] a = {4, 6, 2, 1, 3};
+        int wynik = a[0];
+        int maximum = a[0];
+
+        for (int i=1; i<a.length; i++){
+            if (a[i]>maximum){
+                maximum=a[i];
+            }
+        }
+
+        for (int i=1; i<a.length; i++){
+            if (a[i]<wynik){
+                wynik = a[i];
+            }
+        }
+        System.out.println("element minimalny: "+wynik);
+        System.out.println("element maksymalny: "+maximum);
+
     }
 }
